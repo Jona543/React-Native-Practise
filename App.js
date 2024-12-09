278c1a492a6bb25932d40b39830e213b8f6cf8ca
Jonathan Tromp <jonatromp45@gmail.com>
2024-12-09 21:31:01 +0000

    first phase RN practise

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => {
+    console.log("clicked");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>
+        Open up App.js to start working on app!ascasa sdasdasdas dasdadasdaadsa
+      </Text>
+      <Button
+        color="green"
+        title="click me"
+        onPress={() =>
+          Alert.alert("my title", "my message", [
+            { text: "yes", onPress: () => console.log("yes") },
+            { text: "no", onPress: () => console.log("no") },
+          ])
+        }
+      />
+      <Button
+        title="also click me"
+        onPress={() =>
+          Alert.prompt("My title", "my message", (text) => console.log(text))
+        }
+      />
+      <TouchableNativeFeedback
+        onPress={() => {
+          console.log(handlePress);
+        }}
+      >
+        <Image
+          // fadeDuration={10000}
+          // blurRadius={10}
+          source={{
+            width: 200,
+            height: 200,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableNativeFeedback>
+      <View
+        style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+      ></View>
+      {/* <StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
   },
 });
